55ac6ab91b6113ad8d7040488436d9e7d04d8de9
genkonide <genkonide@gmail.com>
2024-09-27 02:52:26 +0700

    Update urls.py

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path, include
 
-from .views import PostDetailView, PostCategoryView, PostCreateView, PostEditView, PostDeleteView, MainPageView
+from .views import PostDetailView, PostCategoryView, PostCreateView, PostEditView, PostDeleteView, MainPageView, \
+    CommentCreateView, CommentUpdateView, CommentDeleteView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='index'),
-    path('posts/<int:id>/',
-         PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:pk>/',PostDetailView.as_view(), name='post_detail'),
     path('posts/create/', PostCreateView.as_view(), name='create_post'),
-    path('post/<int:post_id>/edit', PostEditView.as_view(), name='post_edit'),
-    path('post/<int:post_id>/delete', PostDeleteView.as_view, name='post_delete'),
-    path('category/<slug:category_slug>/',
-         PostCategoryView.as_view(), name='post_category'),
+    path('posts/<int:post_id>/edit', PostEditView.as_view(), name='edit_post'),
+    path('posts/<int:post_id>/delete', PostDeleteView.as_view, name='delete_post'),
+    path('posts/<int:post_id>/comment/', CommentCreateView.as_view(), name='add_comment'),
+    path('posts/<int:post_id>/edit_comment/<int:comment_id>/', CommentUpdateView.as_view(), name='edit_comment'),
+    path('posts/<int:post_id>/delete_comment/<int:comment_id>/', CommentDeleteView.as_view(), name='delete_comment'),
+    path('category/<slug:category_slug>/',PostCategoryView.as_view(), name='category_posts'),
     path('profile/', include('users.urls')),
 ]
